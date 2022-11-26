d2dec0a535de33042b8a38dcb3f9a466f90f50e8
Philipp Doerner <philippmdoerner@web.de>
2022-11-26 18:30:27 +0100

    FIX dumb syntax mistake

--- src/snorlogue/nimjaTemplateNames.nim
+++ src/snorlogue/nimjaTemplateNames.nim
@@ -1,10 +1,10 @@
 # Model-specific pages
-MODEL_DELETE_PAGE* = "modelDelete.nimja"
-MODEL_LIST_PAGE* = "modelList.nimja"
-MODEL_DETAIL_PAGE* = "modelDetail.nimja"
-MODEL_CREATE_PAGE* = "modelCreate.nimja"
+const MODEL_DELETE_PAGE* = "modelDelete.nimja"
+const MODEL_LIST_PAGE* = "modelList.nimja"
+const MODEL_DETAIL_PAGE* = "modelDetail.nimja"
+const MODEL_CREATE_PAGE* = "modelCreate.nimja"
 
 # general pages
-SQL_PAGE* = "sql.nimja"
-OVERVIEW_PAGE* = "overview.nimja"
-ABOUT_PAGE* = "aboutApplication.nimja"
+const SQL_PAGE* = "sql.nimja"
+const OVERVIEW_PAGE* = "overview.nimja"
+const ABOUT_PAGE* = "aboutApplication.nimja"
