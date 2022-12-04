7f4fdad6ba1bb784bbf5a46b5bdb40eed7635c2c
Philipp Doerner <philippmdoerner@web.de>
2022-12-04 12:02:37 +0100

    ADD test ensuring that superfluous parameters get ignored

--- tests/test_backend_create.nim
+++ tests/test_backend_create.nim
@@ -1,4 +1,4 @@
-import std/[unittest, strformat, httpclient, logging, options]
+import std/[unittest, strformat, httpclient, logging, options, strutils]
 import ./utils/[constants, serverSetup, databaseSetup, responseValidators]
 import ./utils/testModels/[creature]
 
@@ -18,7 +18,7 @@ suite "Testing POST Endpoint":
   test """
     Given a server with snorlogue and a registered Model
     When creating a Model via POST request
-    Then it should return created model and put model into database
+    Then it should put model into database
   """:
     #Given
     var client = newHttpClient()
@@ -287,5 +287,53 @@ suite "Testing POST Endpoint":
 
     #Then
     response.expectHttpCode(500)
+
+
+  test """
+    Given a server with snorlogue and a registered Model
+    When creating a Model via POST request and FormData contains a superfluous additional field like "superfluous-field"
+    Then it should store model in the database and ignore superfluous field
+  """:
+    #Given
+    var client = newHttpClient()
+
+    let model = getDummyCreature()
+    let conn = getServerDbConn()
+    conn.createTables(model)
+
+    #When
+    const superfluousValue =  "I will not show up anywhere"
+    var data: MultipartData = newMultipartData({
+      "request-type": "post",
+      "superfluous-field": superfluousValue,
+      "name": model.name,
+      "description": model.description.get(),
+      "family": $(model.family.int) ,
+      "birthDate": "2022-01-01T00:00",
+      "evilness": $model.evilness,
+      "evilness2": $model.evilness2,
+      "evilness3": $model.evilness3,
+      "count": $model.count,
+      "count2": $model.count2,
+      "count3": $model.count3,
+      "isCool": $model.isCool
+    })
+
+    let url = fmt"{TEST_SERVER_DOMAIN}/admin/creature/"
+    let response = client.post(url, multipart=data)
+
+    #Then
+    response.expectHttpCode(200)
+
+    var allCreatures = @[model]
+    conn.selectAll(allCreatures)
+    check allCreatures.len() == 1
+    check allCreatures[0].name == model.name
+    check allCreatures[0].id > 0
+
+    for fieldName, value in allCreatures[0][].fieldPairs:
+      check superfluousValue notin value.repr
+
+    conn.close()
 
 stopServer()
